c0888eb17ae10625445ee224efec406fdb071b2b
Adrian Åsling Sellius <adrian@gmail.com>
2023-04-12 21:21:51 +0200

    added a fetch for a specific isbn

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-// add your eventlistener here
+async function getBookByISBN() {
+    const resp = await fetch("https://openlibrary.org/isbn/9780140328721.json")
+
+    if (!resp.ok) {
+        throw new Error("Something went wrong when fetching data.")
+    }
+
+    const json = await resp.json()
+
+    return json.title
+}
+
+const app = {
+    data() {
+        return {
+            book: ""
+        }
+    },
+    methods: {
+        async getBook() {
+            book = await getBookByISBN()
+            console.log(book)
+        }
+    }
+}
+Vue.createApp(app).mount("#app")
