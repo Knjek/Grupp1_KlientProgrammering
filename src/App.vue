15bfb464b50e77ac721ed357c1017cb5d37c7994
jessica abony <jessica_abony@hotmail.com>
2023-05-14 21:11:25 +0200

    now the anagram view is showing, also added some kind of error handling and msg stuff

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@ import { RouterView } from 'vue-router'
         <router-link to="/" class="btn btn-outline-primary btn-sm">Home</router-link>
         <router-link to="/max3" class="btn btn-outline-success btn-sm">Max 3 errors</router-link>
         <router-link to="/timer" class="btn btn-outline-warning btn-sm">Timer</router-link>
+        <router-link to="/2player" class="btn btn-outline-danger btn-sm">2-player</router-link>
+        <router-link to="/anagram" class="btn btn-outline-dark btn-sm">Anagram quiz</router-link>
       </nav>
     </div>
     <div>
--- src/views/AnagramView.vue
+++ src/views/AnagramView.vue
@@ -1,10 +1,8 @@
 <script>
-import { RouterLink } from 'vue-router'
 import NameAnagram from '../components/NameAnagram.vue';
 
 export default {
     components: {
-        RouterLink,
         NameAnagram
     }
 }
@@ -12,11 +10,6 @@ export default {
 
 <template>
     <div>
-        <h1>Author anagram</h1>
-        <NameAnagram />
-        <router-link to="/" class="btn btn-outline-success btn-sm">Home</router-link>
-        <router-link to="/max3" class="btn btn-outline-success btn-sm">Max 3 errors</router-link>
-        <router-link to="/2player" class="btn btn-outline-danger btn-sm ">2-player</router-link>
-        <router-link to="/timer" class="btn btn-outline-warning btn-sm">Timer</router-link>
+        <NameAnagram heading="Author anagram quiz!" />
     </div>
 </template>
