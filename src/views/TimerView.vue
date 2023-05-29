7c132d81118bfc4f1984e0561dca85f5b180d3aa
XiaoYanWidheLi <117992468+XiaoYanWidheLi@users.noreply.github.com>
2023-05-29 22:16:22 +0200

    New try with the timer

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -7,8 +7,14 @@ export default {
   },
   data() {
     return {
-      timerCount:0
-
+      timerCount: 0,
+      startButton: false
+    }
+  },
+  methods: {
+    start() {
+      this.timerCount = 10
+      this.startButton = true
     }
   },
   watch: {
@@ -19,6 +25,9 @@ export default {
             this.timerCount--;
           }, 1000);
         }
+        if (value === 0) {
+          this.startButton = false
+        }
       }
     }
   }
@@ -27,10 +36,22 @@ export default {
 
 <template>
   <div>
-    <BooksAndAuthors heading="Guess before your time run's out!" />
+    <!-- hide until press start
+    AND
+    when timer is 0 hide again-->
+    <div id="timer">
+      <button id="startButton" @click="start">Start</button>
 
-    <button id="startButton" @click="timerCount=10">Start</button>
+      <h1 v-if="startButton">
+        <BooksAndAuthors heading="Guess before your time runs out!" />
+      </h1>
+      <h1 v-else>Time out! 😢</h1>
+    </div>
+
+<!--    <BooksAndAuthors  hading="Guess before your time run's out!" />-->
+
+<!--    <button id="startButton" @click="timerCount=10">Start</button>-->
 
     <p>{{ timerCount }} seconds left </p>
   </div>
-</template>*/
+</template>
